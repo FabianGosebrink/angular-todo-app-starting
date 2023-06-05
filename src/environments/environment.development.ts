5d65a081a98a14cd49b9e8d4d2fcac6e6b94f4e0
FabianGosebrink <fabian.gosebrink@outlook.com>
2023-06-05 08:26:11 +0200

    Ading stuff

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {  production: false,
+  apiUrl: `https://sampletodobackend.azurewebsites.net/api/v1/`,
+  serverUrl: `https://sampletodobackend.azurewebsites.net/`,};
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {  production: true,
+  apiUrl: `https://sampletodobackend.azurewebsites.net/api/v1/`,
+  serverUrl: `https://sampletodobackend.azurewebsites.net/`};
