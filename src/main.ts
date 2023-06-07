92ce4e9c9f06b856ae392e15897e62e5b2db6373
FabianGosebrink <fabian.gosebrink@outlook.com>
2023-06-07 15:02:43 +0200

    second step of migration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,38 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { importProvidersFrom, isDevMode } from '@angular/core';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { Routes, provideRouter } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
+const routes: Routes = [
+  {
+    path: 'todo',
+    loadChildren: () =>
+      import('./app/todo/todo.module').then((m) => m.TodoModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./app/about/about.module').then((m) => m.AboutModule),
+  },
+  { path: '', redirectTo: '/todo', pathMatch: 'full' },
+];
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      StoreModule.forRoot({}),
+      EffectsModule.forRoot([]),
+      StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
+    ),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+}).catch((err) => console.error(err));
